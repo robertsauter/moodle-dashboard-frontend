e38cd45c3433450688c6998418170906095e6966
B-Klei <kekadula@gmail.com>
2023-01-21 14:31:27 +0100

    test

--- group_services/planning_service.py
+++ group_services/planning_service.py
@@ -1,32 +1,53 @@
 from lib.sql_handler import SQLHandlerFacade
+import datetime
+import calendar
 
 def operation() -> dict:
-    # handler = SQLHandlerFacade(app=self.app, query="SELECT id, name FROM mdl_assign")
     handler_assign = SQLHandlerFacade(query="SELECT id, name, intro, duedate FROM mdl_assign")
     handler_assign_submission = SQLHandlerFacade(query="SELECT assignment, status, userid FROM mdl_assign_submission")
 
     result_assign, assign_df = handler_assign.operation()
     result_assign_submission, assign_df = handler_assign_submission.operation()
 
-    # operation_result = {"result_assign": result_assign, "result_assign_submission": result_assign_submission}
     operation_result = {"result_assign": result_assign.get("result"),
                         "result_assign_submission": result_assign_submission.get("result")}
 
-    """
-    handler_one_user = SQLHandlerFacade(app=self.app, query="SELECT assignment, status FROM mdl_assign_submission WHERE userid = 15")
-    operation_result, assign_df = handler_one_user.operation()
-    """
-
-    # operation_result, pd_dataframe = handler.operation()
-    """# Work with pandas.
-
-    print(pd_dataframe.head())
-    print(pd_dataframe["id"])
-    print(pd_dataframe.loc[:5, ["id", "grade"]])
-    print(pd_dataframe.iloc[0])
-    print(pd_dataframe[pd_dataframe["grade"] == '8.50000'])
-    print(pd_dataframe[pd_dataframe["timemodified"].isin([1577442881, 1577442882])])
-    """
-    # Work with json data.
     data = operation_result.get("result")
     return operation_result
+
+# Calendar Arrays
+dayAbbr = [y for y in calendar.day_abbr]
+monthAbbr = [w for w in calendar.month_abbr]
+
+# Convert epoch time to deadline
+def deadline(duedate_epoch):
+  duedate_full = str(datetime.datetime.fromtimestamp(duedate_epoch))
+  duedate_year = int(duedate_full[0:4])
+  duedate_month = int(duedate_full[5:7])
+  duedate_day = int(duedate_full[8:10])
+  duedate_time = str(duedate_full)[11:16]
+
+  weekdayNumber = calendar.weekday(duedate_year, duedate_month, duedate_day)
+  weekdayAbbr = dayAbbr[weekdayNumber]
+
+  deadlineStr = "due " + weekdayAbbr + ", " + \
+                str(duedate_day) + " " + \
+                monthAbbr[duedate_month] + " " + \
+                str(duedate_year) + " " + \
+                str(duedate_time)
+  return deadlineStr
+
+# List of assignments
+def assignmentsToDisplay(user, dataset):
+  assignmentsList = []
+
+  for submission in dataset["result_assign_submission"]: # going through
+    statusDict = {}
+    if submission["userid"] == user: # pick submissions from the user
+      for assignment in dataset["result_assign"]: #going through
+        if submission["assignment"] == assignment["id"]: # pick the assignment
+          statusDict["status"] = submission["status"] # status as a dictionary
+          assignment.update(statusDict) # append status to assignment
+          assignmentsList.append(assignment) # append dict to the list
+
+  return(assignmentsList)
--- pages/planning.py
+++ pages/planning.py
@@ -1,11 +1,8 @@
 import dash
-import requests
 from dash import html#, dcc
-#import plotly.graph_objects as go
 import dash_bootstrap_components as dbc
-import datetime
-import calendar
-from group_services.planning_service import operation
+from group_services.planning_service import *
+from dash.dependencies import Input, Output
 
 
 dash.register_page(__name__,
@@ -14,62 +11,15 @@ dash.register_page(__name__,
                    title='Planning')
 
 
-# This is the JSON object, that you can use to display your visualizations :)
+# Our data
 data = operation()
 
-# Calendar Arrays
-dayNames = [x for x in calendar.day_name]
-dayAbbr = [y for y in calendar.day_abbr]
-monthNames = [z for z in calendar.month_name]
-monthAbbr = [w for w in calendar.month_abbr]
+# Done?
+assignmentDone = "default"
 
 
-# Def
-def deadline(duedate_epoch):
-  duedate_full = str(datetime.datetime.fromtimestamp(duedate_epoch))
-  duedate_year = int(duedate_full[0:4])
-  duedate_month = int(duedate_full[5:7])
-  duedate_day = int(duedate_full[8:10])
-
-  weekdayNumber = calendar.weekday(duedate_year, duedate_month, duedate_day)
-  weekdayAbbr = dayAbbr[weekdayNumber]
-
-  deadlineStr = "due " + weekdayAbbr + ", " + str(duedate_day) + " " + monthAbbr[duedate_month] + " " + str(duedate_year)
-  return deadlineStr
-
-
-# This is a dummy list of assignments, that we can iterate over to display on the page
-"""assignments = [
-    {'title': 'Assigment 1', 'desc': 'Blablabla', 'done': True},
-    {'title': 'Assigment 2', 'desc': 'Blablabla', 'done': True},
-    {'title': 'Assigment 3', 'desc': 'Blablabla', 'done': False},
-    {'title': 'Assigment 4', 'desc': 'Blablabla', 'done': False},
-    {'title': 'Assigment 5', 'desc': 'Blablabla', 'done': False}
-]"""
-# This is a python dict, that can be used to create a bar chart
-"""dict_figure = {
-    'data': [
-        {
-            'x': ['Assignment 1', 'Assignment 2', 'Assignment 3'],
-            'y': [10, 1, 5],
-            'type': 'bar',
-            'name': 'Grades'
-        }
-    ],
-    'layout': {
-        'title': 'Assignment grades'
-    }
-}
-# This is a plotly graph object to create the same bar chart
-graph_object_figure = go.Figure(
-    data=[go.Bar(x=['Assignment 1', 'Assignment 2', 'Assignment 3'], y=[10, 1, 5])],
-    layout=go.Layout(
-        title=go.layout.Title(text='Assignment grades')
-    )
-)
+# Layout
 """
-
-# This is the html layout, that is displayed on the page
 layout = html.Div(children=[
     html.H1(
         'Planning page',
@@ -89,22 +39,56 @@ layout = html.Div(children=[
                             html.P(assignment['intro'], className='card-text', style={'margin': '2rem'}),
 
                         ]
-                    ), className='done'
+                    ), className=assignmentDone #className='done'
                     #className='done' if assignment['done'] else ''
                 ),
                 style={'marginBottom': '1rem'}
 
-            ) for assignment in data["result_assign"]
+            ) for assignment in assignmentsToDisplay(1, data)
         ],
         style={'listStyle': 'none', 'padding': '0', 'marginBottom': '5rem'}
-    ),
-    """
-    html.H2('Simple chart from a python dict'),
-    dcc.Graph(
-        figure=dict_figure,
-        style={'marginBottom': '5rem'}
-    ),
-    html.H2('Simple chart from a plotly graph object'),
-    dcc.Graph(figure=graph_object_figure)
-"""
+    )
 ])
+"""
+layout = html.Div(children=[
+    html.H1(
+        'Planning page',
+        style={'marginBottom': '2rem'}
+    ),
+    html.H2('To do:'),
+    html.Div(id="assignmentsDisplayed")
+])
+@dash.callback(
+    Output('assignmentsDisplayed', 'children'),
+    Input('userId', 'data'),
+)
+def fetch_data_on_user_select(user_id):
+
+    #assignmentsForUser = assignmentsToDisplay(user_id, data)
+
+    content = [
+        html.Ul(
+            [
+                html.Li(
+                    dbc.Card(
+                        dbc.CardBody(
+                            [
+                                html.H4(assignment['name'], className='card-title'),
+                                html.Img(src='assets/check2-circle.svg',
+                                         style={'position': 'absolute', 'top': '2rem', 'right': '2rem'}),
+                                html.H5(deadline(assignment['duedate']), className='deadline duedate'),
+                                html.P(assignment['intro'], className='card-text', style={'margin': '2rem'}),
+
+                            ]
+                        ), className=assignmentDone  # className='done'
+                        # className='done' if assignment['done'] else ''
+                    ),
+                    style={'marginBottom': '1rem'}
+
+                ) for assignment in assignmentsToDisplay(user_id, data) #assignmentsForUser
+            ],
+            style={'listStyle': 'none', 'padding': '0', 'marginBottom': '5rem'}
+        )
+    ]
+    return content
+
