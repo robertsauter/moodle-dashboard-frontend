d5aad4e647ecac46690b4defabbc13d149b18329
Raphael Stedler <r.stedler@gmx.de>
2023-01-23 20:03:41 +0100

    overall_progress adapted

--- pages/overall_progress.py
+++ pages/overall_progress.py
@@ -272,3 +272,225 @@ def fetch_selected_progress(user_id):
                         dbc.Row(children=[file_table1,file_table2], style={"height": "65vh"})],width=6)
                 ])])])
 
+'''
+# marlas quiz table (both columns in one table)
+quiz_data = pd.DataFrame({'Completed':user_all_activities[user_all_activities['Component'] == "Quiz"]['Event context'],
+                          'Uncompleted':unseen[unseen['Component'] == "Quiz"]['Event context']})
+quiz_table = dash_table.DataTable(
+    id='quiz_table',
+    columns= [{"name": i, "id": i, "type": "text"}
+                   for i in quiz_data.columns],
+    data=quiz_data.to_dict('list'),
+    style_cell={
+        'textAlign':'left',
+        'width': '{}%'.format(len(quiz_data.columns)),
+        'textOverflow': 'ellipsis',
+        'overflowY': 'auto',
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+    style_header=dict(backgroundColor="paleturquoise"),
+    style_data=dict(backgroundColor="lavender"),
+    style_table={
+            'maxHeight': '100%',
+            'maxWidth': 'width=550px',
+            'overflowY': 'auto',
+            'margin': '25px',
+            },
+    css=[{'selector': 'table', 'rule': 'table-layout: fixed'}],
+    )
+'''
+
+# section for assignments
+# datatable for completed assignment
+
+
+'''
+# Marlas assign datatable(both columns  in one datatable)
+assign_data = pd.DataFrame({'Completed':user_all_activities[user_all_activities['Component'] == "Assignment"]['Event context'],
+                            'Uncompleted':unseen[unseen['Component'] == "Assignment"]['Event context']})
+assign_values = assign_data.values
+print(assign_values)
+i = assign_data.index
+for x, y in assign_values:
+    print('x:' , x)
+    print('y:' , y)
+    if not isinstance(y, str):
+        print('ya')
+print(assign_data.to_dict())
+assign_table = dash_table.DataTable(
+    id='assign_table',
+    columns=[{"name": i, "id": i, "type": "text",'presentation':"markdown"}
+        for i in assign_data.columns],
+    data=assign_data.to_dict('records'),
+    style_cell=
+    {'textAlign':'left',
+        'width': '{}%'.format(len(assign_data.columns)),
+        'textOverflow': 'ellipsis',
+        'overflowY': 'auto',
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+    style_header=dict(backgroundColor="paleturquoise"),
+    style_data=dict(backgroundColor="lavender"),
+    style_table={
+            'maxHeight': '100%',
+            'maxWidth': 'width=550px',
+            'overflowY': 'auto',
+            'margin': '25px',
+            },
+    css=[{'selector': 'table', 'rule': 'table-layout: fixed'}],
+    )
+'''
+# section for URL Data
+
+
+# first url data table(seen)
+
+
+
+'''
+# marlas url table
+url_data = pd.DataFrame({'Seen':user_all_activities[user_all_activities['Component'] == "URL"]['Event context'],
+                            'Unseen':unseen[unseen['Component'] == "File"]['Event context']})
+url_table = dash_table.DataTable(
+    id='url_table',
+    columns=[{"name": i, "id": i, 'presentation':'markdown'}
+        for i in url_data.columns],
+    data=url_data.to_dict('records'),
+    style_cell={
+        'textAlign':'left',
+        'width': '{}%'.format(len(url_data.columns)),
+        'textOverflow': 'ellipsis',
+        'overflowY': 'auto',
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+    style_header=dict(backgroundColor="paleturquoise"),
+    style_data=dict(backgroundColor="lavender"),
+    style_table={
+            'maxHeight': '100%',
+            'maxWidth': 'width=550px',
+            'overflowY': 'auto',
+            'margin': '25px',
+            },
+    css=[{'selector': 'table', 'rule': 'table-layout: fixed'}],
+    )
+'''
+# section for File Data
+# first file datatable(seen)
+
+
+
+
+'''
+#  marlas file datatable (2 columns in one table)
+file_data = pd.DataFrame()
+file_data['Completed'] = user_all_activities[user_all_activities['Component'] == "File"]['Event context']
+file_data['Uncompleted'] = unseen[unseen['Component'] == "File"]['Event context']
+print(file_data.to_dict())
+file_table = dash_table.DataTable(
+    id='file_table',
+    columns=[{"name": i, "id": i, 'presentation':'markdown'}
+        for i in file_data.columns],
+    data=file_data.to_dict('records'),
+    style_cell={
+        'textAlign':'left',
+        'width': '{}%'.format(len(file_data.columns)),
+        'textOverflow': 'ellipsis',
+        'overflowY': 'auto',
+        'whiteSpace': 'normal',
+        'height': 'auto',
+    },
+    style_header=dict(backgroundColor="paleturquoise"),
+    style_data=dict(backgroundColor="lavender"),
+    style_table={
+            'maxHeight': '100%',
+            'maxWidth': 'width=550px',
+            'overflowY': 'auto',
+            'margin': '25px',
+            },
+    css=[{'selector': 'table', 'rule': 'table-layout: fixed'}],
+    )
+'''
+
+''' implementation of the dash graphic object(go) table
+fig5 = go.Figure(data=[go.Table(
+    header=dict(values=['<b> Completed</b>', '<b>Not Completed</b>'],
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[user_all_activities[user_all_activities['Component'] == "Assignment"]['Event context'],
+                       unseen[unseen['Component'] == "Assignment"]['Event context']],
+               fill_color='lavender',align='left'))
+])
+fig5.update_layout(width=550, height=250, autosize=False, margin=dict(l=15, r=15, b=5, t=5))
+fig6 = go.Figure(data=[go.Table(
+    header=dict(values=['<b> Seen</b>', '<b>Not Seen</b>'],
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[user_all_activities[user_all_activities['Component'] == "URL"]['Event context'],
+                       unseen[unseen['Component'] == "URL"]['Event context']],
+               fill_color='lavender',
+               align='left'))
+])
+fig6.update_layout(width=550, height=250, autosize=False, margin=dict(l=15, r=25, b=5, t=5))
+fig7 = go.Figure(data=[go.Table(
+    header=dict(values=['<b> Seen</b>', '<b>Not Seen</b>'],
+                fill_color='paleturquoise',
+                align='left'),
+    cells=dict(values=[user_all_activities[user_all_activities['Component'] == "File"]['Event context'],
+                       unseen[unseen['Component'] == "File"]['Event context']],
+               fill_color='lavender',
+               align='left'))
+])
+fig7.update_layout(width=550, height=250, autosize=False, margin=dict(l=15, r=15, b=5, t=5))'''
+
+
+
+# the default code written by infrastructure group
+# Value for the progress bar
+# progress = 50
+# This is a python dict, that can be used to create a bar chart
+"""dict_figure = {
+    'data': [
+        {
+            'x': ['Assignment 1', 'Assignment 2', 'Assignment 3'],
+            'y': [10, 1, 5],
+            'type': 'bar',
+            'name': 'Grades'
+        }
+    ],
+    'layout': {
+        'title': 'Assignment grades'
+    }
+}"""
+# This is a plotly graph object to create the same bar chart
+"""graph_object_figure = go.Figure(
+    data=[go.Bar(x=['Assignment 1', 'Assignment 2', 'Assignment 3'], y=[10, 1, 5])],
+    layout=go.Layout(
+        title=go.layout.Title(text='Assignment grades')
+    )
+)"""
+
+
+# This is the html layout, that is displayed on the page
+# I found this progressbar component from bootstrap. Maybe it's helpful for you?
+"""layout = html.Div(children=[
+    html.H1(
+        'Overall progress page',
+        style={'marginBottom': '2rem'}
+    ),
+    html.H2('Just a simple progress bar from bootstrap'),
+    dbc.Progress(
+        value=progress,
+        label=f'{progress}%',
+        style={'marginBottom': '5rem'}
+    ),
+    html.H2('Simple chart from a python dict'),
+    dcc.Graph(
+        figure=dict_figure,
+        style={'marginBottom': '5rem'}
+    ),
+    html.H2('Simple chart from a plotly graph object'),
+    dcc.Graph(figure=graph_object_figure)
+])"""
