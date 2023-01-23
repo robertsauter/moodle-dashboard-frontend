bdf5dc95b2f4cd402c55c4d66cc9cf2620bbf1fc
Raphael Stedler <r.stedler@gmx.de>
2023-01-23 11:21:48 +0100

    updated

--- pages/planning.py
+++ pages/planning.py
@@ -1,12 +1,8 @@
-
 import dash
-import requests
-from dash import html#, dcc
-#import plotly.graph_objects as go
+from dash import html
 import dash_bootstrap_components as dbc
-import datetime
-import calendar
-from group_services.planning_service import operation
+from group_services.planning_service import *
+from dash.dependencies import Input, Output
 import re
 
 dash.register_page(__name__,
@@ -14,7 +10,8 @@ dash.register_page(__name__,
                    name='Planning',
                    title='Planning')
 
-# This is the JSON object, that you can use to display your visualizations :)
+
+# Our data
 data = operation()
 
 # HTML Cleaner to remove tags
@@ -23,54 +20,48 @@ def cleanhtml(raw_html):
   cleantext = re.sub(CLEANR, '', raw_html)
   return cleantext
 
-# Calendar Arrays
-dayNames = [x for x in calendar.day_name]
-dayAbbr = [y for y in calendar.day_abbr]
-monthNames = [z for z in calendar.month_name]
-monthAbbr = [w for w in calendar.month_abbr]
-
-
-# Def
-def deadline(duedate_epoch):
-  duedate_full = str(datetime.datetime.fromtimestamp(duedate_epoch))
-  duedate_year = int(duedate_full[0:4])
-  duedate_month = int(duedate_full[5:7])
-  duedate_day = int(duedate_full[8:10])
-
-  weekdayNumber = calendar.weekday(duedate_year, duedate_month, duedate_day)
-  weekdayAbbr = dayAbbr[weekdayNumber]
-
-  deadlineStr = "due " + weekdayAbbr + ", " + str(duedate_day) + " " + monthAbbr[duedate_month] + " " + str(duedate_year)
-  return deadlineStr
-
-
-# This is the html layout, that is displayed on the page
+# Layout
 layout = html.Div(children=[
     html.H1(
         'Planning page',
         style={'marginBottom': '2rem'}
     ),
     html.H2('To do:'),
-    html.Ul(
-        [
-            html.Li(
-                dbc.Card(
-                    dbc.CardBody(
-                        [
-                            html.H4(assignment['name'], className='card-title'),
-                            html.Img(src='assets/check2-circle.svg',
-                                     style={'position': 'absolute', 'top': '2rem', 'right': '2rem'}),
-                            html.H5(deadline(assignment['duedate']), className='deadline duedate'),
-                            html.P(cleanhtml(assignment['intro']), className='card-text', style={'margin': '2rem'}),
-
-                        ]
-                    ), className='done'
-                    #className='done' if assignment['done'] else ''
-                ),
-                style={'marginBottom': '1rem'}
-
-            ) for assignment in data["result_assign"]
-        ],
-        style={'listStyle': 'none', 'padding': '0', 'marginBottom': '5rem'}
-    )
+    html.Div(id="assignmentsDisplayed")
 ])
+
+@dash.callback(
+    Output('assignmentsDisplayed', 'children'),
+    Input('userId', 'data'),
+)
+def fetch_data_on_user_select(user_id):
+
+    if len(assignmentsToDisplay(user_id, data)) > 0:
+        content = [
+            html.Ul(
+                [
+                    html.Li(
+                        dbc.Card(
+                            dbc.CardBody(
+                                [
+                                    html.H4(assignment['name'], className='card-title'),
+                                    html.Img(src='assets/check2-circle.svg',
+                                             style={'position': 'absolute', 'top': '2rem', 'right': '2rem'}),
+                                    html.H5(deadline(assignment['duedate']), className='deadline duedate'),
+                                    html.P(cleanhtml(assignment['intro']), className='card-text', style={'margin': '2rem'}),
+
+                                ]
+                            ), className='done' if (assignment["status"] == "submitted") else 'unfinished' if int(assignment["duedate"]) < currentDate() else ""
+                        ),
+                        style={'marginBottom': '1rem'}
+
+                    ) for assignment in assignmentsToDisplay(user_id, data)
+                ],
+                style={'listStyle': 'none', 'padding': '0', 'marginBottom': '5rem'}
+            )
+        ]
+
+    else:
+        content = [html.P("There are no assignments due.")]
+
+    return content
