730a781d9e742eab4768492aac85ab19c63a30fd
Rojal <Rojal.mohammad1@gmail.com>
2023-01-23 18:00:21 +0100

    assessment frontend (v.3 faster version)

--- pages/assessment.py
+++ pages/assessment.py
@@ -34,21 +34,20 @@ def fetch_selected_assessment(user_id):
     quiz_grades_df_edited = pd.read_json(data[0])
     assign_edited = pd.read_json(data[1])
 
-    # just a checker
-    #print(quiz_grades_df_edited)
-    #print(assign_edited)
-
     # This is a plotly graph object to create the same bar chart
-
     # creating bar chart for quiz
     fig1 = px.bar(quiz_grades_df_edited, x='quiz', y='grade',
                 labels={'quiz': '<b> quiz name <b>', 'grade': '<b> your grade <b>'}
-                , title='Quiz Grades', color='quiz', text='grade', range_y=[0, 12])
+                ,color='quiz', text='grade', range_y=[0, 12]
+                #, title='Quiz Grades'
+                )
 
     # creating bar chart for assignment
     fig2 = px.bar(assign_edited, x='assignment', y='grade',
                 labels={'assignment': '<b> assignment name <b>', 'grade': '<b> your grade <b>'}
-                , title='<b> Assignment Grades <b>', color='assignment', text='grade', range_y=[0, 12])
+                , color='assignment', text='grade', range_y=[0, 12]
+                #, title='<b> Assignment Grades <b>'
+                )
 
     # changing the quiz text size and background
     fig1.update_traces(texttemplate='%{text:.2s}', textposition='outside')
@@ -76,27 +75,21 @@ def fetch_selected_assessment(user_id):
         legend_title_font_color="green"
     )
 
-    # Update xaxis properties
+    # Update x-axis properties
     fig2.update_xaxes(tickfont_size=15, title_font_family="Arial", tickwidth=10)
     fig1.update_xaxes(tickfont_size=12, title_font_family="Arial", tickwidth=10, showgrid=False)
 
-    # Update yaxis properties
-    fig1.update_yaxes(tickfont_size=11, title_font_family="Arial", tickwidth=10)
-    fig2.update_yaxes(tickfont_size=11, title_font_family="Arial", tickwidth=10, showgrid=False)
+    # Update y-axis properties
+    fig1.update_yaxes(tickfont_size=1, title_font_family="Arial", tickwidth=10)
+    fig2.update_yaxes(tickfont_size=1, title_font_family="Arial", tickwidth=10, showgrid=False)
 
-    '''
-    Todo: 
-        1-Feedback messages depending on the actual grades
-        2-Heat-Map (to not show grades directly to the user)
-    '''
+    # name of quizzes
+    attended_quizzes = data[2]
 
-    # fetching all Quizzes grades of the given user if not available then 0
-    # quizes_all = ['quiz 1', 'quiz 2', 'quiz 3', 'quiz 4', 'quiz 5', 'quiz 6', 'quiz final']
-    quizes_all = ['quiz 1', 'quiz 2', 'quiz 3', 'quiz 4', 'quiz 5', 'quiz 6', 'quiz 7', 'quiz 8', 'quiz 9']
     grades = []
-    for i in range(len(quizes_all)):
+    for i in attended_quizzes:
         try:
-            value = int(quiz_grades_df_edited.loc[quiz_grades_df_edited['quiz'] == quizes_all[i], 'grade'].item())
+            value = int(quiz_grades_df_edited.loc[quiz_grades_df_edited['quiz'] == i, 'grade'].item())
             try:
                 grades.append(value)
             except:
@@ -104,42 +97,68 @@ def fetch_selected_assessment(user_id):
         except:
             grades.append(0)
 
-    # creating Quiz Feedback
-    quiz_feedback = ""
+    # the number of all quizzes
+    total_quizzes_nums0 = data[3]
 
+    # convert the quiz's from string to int
+    total_quizzes_nums = [int(i) for i in total_quizzes_nums0]
+
+    # number of attended quizzes 6
+    num_attended_quizzes = len(attended_quizzes)
+
+    # total number of the quizzes  9
+    all_quizzes_nums = len(total_quizzes_nums)
 
     # counting missed attendees of the Quizzes
-    missedQuizes = 0
-    for i in grades:
-        if i == 0:
-            missedQuizes += 1
-            quiz_feedback = "you have missed the Quizzes " + str(missedQuizes) + " times.  "
+    missed_quizzes = all_quizzes_nums - num_attended_quizzes
+
+    # creating Quiz Feedback
+    quiz_feedback = "you have missed the Quizzes " + str(missed_quizzes) + " times.  "
 
     # calculating the total assignment grades
-    totalGrade = reduce(add, grades)
-    #print(totalGrade)
+    try:
+        # reduce type error by empty values
+        total_grade = reduce(add, grades)
+    except TypeError:
+        print("empty user value")
+    except:
+        pass
+
+    # summing by 2. elements in list // by using, will lower feedback failure
+    grades_2_in_row = [sum(grades[i:i + 2]) for i in range(0, len(grades), 2)]
+    # summing by 3. elements in list // can be used with help of the temp List
+    grades_3_in_row = [sum(grades[i:i + 3]) for i in range(0, len(grades), 3)]
+
+    grade_of_each_quiz = 10
+    all_quizzes_grades = all_quizzes_nums * grade_of_each_quiz  # 30
+
+    total_per_grade = all_quizzes_grades / 100
+    missed_per_quiz = all_quizzes_nums / 100
 
     # Feedback possibilities for Quizzes
-    if grades[0] + grades[1] + grades[2] >= 22 and grades[3] + grades[4] + grades[5] >= 22 and grades[
-        6] >= 6 or totalGrade >= 50:
+    if missed_quizzes <= 40 * missed_per_quiz and total_grade >= 60 * total_per_grade:
         quiz_feedback += " you are doing very good keep going."
-    elif 15 <= grades[0] + grades[1] + grades[2] < 22 and 15 < grades[3] + grades[4] + grades[5] and 4 <= grades[6] < 6:
+    elif missed_quizzes <= 50 * missed_per_quiz and total_grade >= 45 * total_per_grade:
         quiz_feedback += "you are still doing well but focus more on your materials and study more to achieve better " \
-                        "results."
-    elif 10 <= grades[0] + grades[1] + grades[2] < 15 and 10 <= grades[3] + grades[4] + grades[5] and 2 <= grades[6] < 4:
+                         "results."
+    elif missed_quizzes <= 65 * missed_per_quiz and total_grade >= 35 * total_per_grade:
         quiz_feedback += "your grades is not looking very good. You need to focus more or get help from your Professor."
     else:
-        quiz_feedback += " contact your professor."
-
+        quiz_feedback += "contact your professor."
 
     # fetching all Assignment grades of the given user if not available then 0
-    # assem_all = ['AS1 - W3', 'AS2 - W5', 'AS3 - W10', 'AS4 - W11']
-    assem_all = ['AS1', 'AS2', 'AS3', 'AS4', 'AS5', 'AS6', 'AS7', 'AS8', 'AS9', 'AS10',
-    'AS11', 'AS12', 'AS13', 'AS14']
+    attended_assignment = data[4]
+
+    # the number of all Assignments [9,12,14]
+    total_assignment_nums0 = data[5]
+
+    # convert the Assignments from string to int
+    total_assignment_nums = [int(i) for i in total_assignment_nums0]
+
     assa_grades = []
-    for i in range(len(assem_all)):
+    for i in attended_assignment:
         try:
-            value = int(assign_edited.loc[assign_edited['assignment'] == assem_all[i], 'grade'].item())
+            value = int(assign_edited.loc[assign_edited['assignment'] == i, 'grade'].item())
             try:
                 assa_grades.append(value)
             except:
@@ -147,32 +166,42 @@ def fetch_selected_assessment(user_id):
         except:
             assa_grades.append(0)
 
+    # number of attended quizzes
+    num_attended_assignment = len(attended_assignment)
 
-    # creating Assignment Feedback
-    assa_feedback = ""
+    # total number of the quizzes
+    number_all_assignment = len(total_assignment_nums)
 
-    # counting missed attendees of the Assignment
-    missed_assa = 0
-    for i in assa_grades:
-        if i == 0:
-            missed_assa += 1
-            assa_feedback = "you have missed the Assignment " + str(missed_assa) + " times.  "
+    # counting missed attendees of the Quizzes
+    missed_assignment = number_all_assignment - num_attended_assignment
 
     # calculating the total assignment grades
-    totalasGrade = reduce(add, assa_grades)
-    #print(totalasGrade)
+    try:
+        total_ass_grades = reduce(add, assa_grades)
+    except TypeError:
+        print("empty user value")
+    except:
+        pass
+
+    grade_of_each_assignment = 10
+    all_assignment_grades = number_all_assignment * grade_of_each_assignment
+
+    total_per_grade_assignment = all_assignment_grades / 100
+    missed_per_assignment = number_all_assignment / 100
+
+    # creating Assignment Feedback
+    assa_feedback = "you have missed the Quizzes " + str(missed_assignment) + " times.  "
 
     # Feedback possibilities for assignment
-    if assa_grades[0] + assa_grades[1] >= 14 and assa_grades[2]+assa_grades[3] >= 14 or totalasGrade >= 28:
+    if missed_assignment <= 40 * missed_per_assignment and total_ass_grades >= 60 * total_per_grade_assignment:
         assa_feedback += " you are doing very good keep going."
-    elif 10 <= assa_grades[0] + assa_grades[1] < 14 and 10 <= assa_grades[2]+assa_grades[3] < 14 or totalasGrade >= 24:
-        assa_feedback += " you are still doing well but focus more on your materials and study more to achieve better " \
-                        "results."
-    elif 8 <= assa_grades[0] + assa_grades[1] < 10 and 8 <= assa_grades[2]+assa_grades[3] < 10 or totalasGrade >= 20:
-        assa_feedback += " your grades is not looking very good. You need to focus more or get help from your Professor."
+    elif missed_assignment <= 50 * missed_per_assignment and total_ass_grades >= 45 * total_per_grade_assignment:
+        assa_feedback += "you are still doing well but focus more on your materials and study more to achieve better " \
+                         "results."
+    elif missed_assignment <= 65 * missed_per_assignment and total_ass_grades >= 35 * total_per_grade_assignment:
+        assa_feedback += "your grades is not looking very good. You need to focus more or get help from your Professor."
     else:
-        quiz_feedback += " contact your professor."
-
+        assa_feedback += "contact your professor."
 
     # This is the html layout, that is displayed on the page
     return html.Div(children=[
@@ -194,6 +223,8 @@ def fetch_selected_assessment(user_id):
             target="popover-target",
             trigger="click",
         ),
+
+        html.Br(), html.Br(), html.Br(), html.Br(),
 
         html.H2('Assignment Grades'),
         dcc.Graph(figure=fig2),
